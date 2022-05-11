462bd2e55ac562e9700e2141acacc620857617e0
Dguykc1 <kezechristian@gmail.com>
2022-05-11 09:10:51 +0100

    deployment of n_creationz

---
+++ src/main/java/codeblaze/com/n_creationz/NCreationzApplication.java
@@ -0,0 +1,13 @@
+package codeblaze.com.n_creationz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NCreationzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NCreationzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeblaze/com/n_creationz/NCreationzApplicationTests.java
@@ -0,0 +1,13 @@
+package codeblaze.com.n_creationz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NCreationzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
