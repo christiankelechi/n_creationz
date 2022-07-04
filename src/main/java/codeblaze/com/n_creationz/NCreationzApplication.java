fbe3de8e7906406c9b6164c9e1ef72dd6f6d0fbc
Dguykc1 <kezechristian@gmail.com>
2022-07-04 18:25:04 +0100

    edited some areas

--- src/main/java/codeblaze/com/n_creationz/NCreationzApplication.java
+++ src/main/java/codeblaze/com/n_creationz/NCreationzApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NCreationzApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NCreationzApplication.class, args);
 	}
